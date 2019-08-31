28791e74116e4e81f1672eb252c4302fda7969de
ebedoyaalzate <esteban.bedoyaa@udea.edu.co>
2019-08-31 12:01:27 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'codeBreakerFront';
+  secret: string;
+  number: string;
+  result: string;
+  message;
+
+  constructor(private http: HttpClient) {}
+
+  setSecret() {
+    this.http.get<any>('http://localhost:3000/setsecret/'+this.secret).subscribe(res => {
+          this.message = res.message
+        })
+  }
+
+  guessNumber() {
+    this.http.get<any>('http://localhost:3000/guess/'+this.number).subscribe(res => {
+          this.result = res.result
+        })
+  }
 }
